e8493e1a5c03bbb4483255227a38bac304c69c1e
phatlam1811 <phatlam1811@gmail.com>
2023-02-22 19:18:41 +0700

    load & display users data

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,27 @@
+import connection from "../configs/dbSettings.js";
+
 // GET
 export const homePage = (req, res) => {
-    return res.render(`index.ejs`);
+    connection.connect((err) => {
+        if (err) console.log(err);
+    })
+
+    connection.query(
+        'select * from Users', (err, results, fields) => {
+            let data = [];
+
+            if (err) throw err;
+
+            results.map((row) => {
+                data.push({
+                    _id: row._id,
+                    firstName: row.firstName,
+                    lastName: row.lastName,
+                    email: row.email,
+                    address: row.address
+                })
+            })
+
+            return res.render(`index.ejs`, { usersData: data });
+        })
 }
--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import configurate from "./configs/viewEngine.js";
 import { config } from "dotenv";
 import initWebRoute from "./routes/webRoutes.js";
-import connection from "./configs/dbSettings.js";
 
 config();
 
@@ -11,18 +10,6 @@ const port = process.env.port || 3000;
 
 configurate(app);
 initWebRoute(app);
-connection.connect((err) => {
-    if (err)
-        console.log(err);
-    else
-        console.log('connected!');
-})
-
-connection.query(
-    'select * from Users', (err, res, fields) => {
-        console.log(res)
-    }
-)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
