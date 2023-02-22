a646e94c4b58fbc78930b49ba3974335e6a419d9
phatlam1811 <phatlam1811@gmail.com>
2023-02-23 02:54:25 +0700

    using mysql2 promise (query to execute)

--- src/configs/dbSettings.js
+++ src/configs/dbSettings.js
@@ -1,6 +1,6 @@
-import mysql from "mysql2"
+import mysql from "mysql2/promise"
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'nodejstutorial'
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,27 +1,8 @@
 import connection from "../configs/dbSettings.js";
 
 // GET
-export const homePage = (req, res) => {
-    connection.connect((err) => {
-        if (err) console.log(err);
-    })
+export const homePage = async (req, res) => {
+    const [results, fields] = await connection.execute('select * from Users')
 
-    connection.query(
-        'select * from Users', (err, results, fields) => {
-            let data = [];
-
-            if (err) throw err;
-
-            results.map((row) => {
-                data.push({
-                    _id: row._id,
-                    firstName: row.firstName,
-                    lastName: row.lastName,
-                    email: row.email,
-                    address: row.address
-                })
-            })
-
-            return res.render(`index.ejs`, { usersData: data });
-        })
+    return res.render(`index.ejs`, { usersData: results });
 }
