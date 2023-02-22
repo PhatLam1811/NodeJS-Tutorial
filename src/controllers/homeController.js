fb5db75246f505700321e850c5e4a285e738453b
phatlam1811 <phatlam1811@gmail.com>
2023-02-23 03:52:37 +0700

    detail & about page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,3 +6,14 @@ export const homePage = async (req, res) => {
 
     return res.render(`index.ejs`, { usersData: results });
 }
+
+// GET
+export const detailsPage = async (req, res) => {
+    // get user id from request
+    const userId = req.params.userId;
+
+    // read from database
+    const [results, fields] = await connection.execute(`select * from Users where _id = ${userId}`);
+
+    return res.render(`details.ejs`, { user: results[0] })
+}
--- src/routes/webRoutes.js
+++ src/routes/webRoutes.js
@@ -1,13 +1,13 @@
 import express from "express"
-import { homePage } from "../controllers/homeController.js"
+import { detailsPage, homePage } from "../controllers/homeController.js"
 import { aboutPage } from "../controllers/aboutController.js";
 
 const router = express.Router();
 
 const initWebRoute = (app) => {
-
     router.get(`/`, homePage);
     router.get(`/about`, aboutPage);
+    router.get('/details/user/:userId', detailsPage);
 
     app.use(`/`, router);
 }
