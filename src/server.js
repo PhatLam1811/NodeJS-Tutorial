ab3fc631a72af703cce8f5679b2f59e17bca3524
phatlam1811 <phatlam1811@gmail.com>
2023-02-22 17:57:34 +0700

    connect to mysql

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import configurate from "./configs/viewEngine.js";
 import { config } from "dotenv";
 import initWebRoute from "./routes/webRoutes.js";
+import connection from "./configs/dbSettings.js";
 
 config();
 
@@ -10,6 +11,18 @@ const port = process.env.port || 3000;
 
 configurate(app);
 initWebRoute(app);
+connection.connect((err) => {
+    if (err)
+        console.log(err);
+    else
+        console.log('connected!');
+})
+
+connection.query(
+    'select * from Users', (err, res, fields) => {
+        console.log(res)
+    }
+)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
