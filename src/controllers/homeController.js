3ffe644f94f071876804337c7b55ac5a78304220
phatlam1811 <phatlam1811@gmail.com>
2023-02-24 03:47:19 +0700

    edit user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -12,12 +12,25 @@ export const detailsPage = async (req, res) => {
     // console.log(userId);
 
     // get user id from request
-    const userId = req.params.userId;
+    let userId = req.params.userId;
 
     // read from database
     const [results, fields] = await connection.execute(`select * from Users where _id = ${userId}`);
 
     return res.render(`details.ejs`, { user: results[0] });
+}
+
+// GET
+export const userEditPage = async (req, res) => {
+    // console.log('edit user id:', req.params.userId);
+
+    // get user id from request
+    let userId = req.params.userId;
+
+    // read from database
+    const [results, field] = await connection.execute('select * from Users where _id = ?', [userId]);
+
+    return res.render('edit.ejs', { user: results[0] });
 }
 
 // POST
@@ -43,7 +56,18 @@ export const deleteUser = async (req, res) => {
 
 // POST
 export const editUser = async (req, res) => {
-    console.log('edit user request body: ', req.body);
+    // console.log('edit user request body: ', req.body);
+
+    let {
+        id,
+        firstName,
+        lastName,
+        email,
+        address
+    } = req.body;
+
+    await connection.execute('update Users set firstName = ?, lastName = ?, email = ?, address = ? where _id = ?',
+        [firstName, lastName, email, address, id]);
 
     return res.redirect('/');
 }
--- src/routes/webRoutes.js
+++ src/routes/webRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { editUser, deleteUser, createUser, detailsPage, homePage } from "../controllers/homeController.js"
+import { userEditPage, editUser, deleteUser, createUser, detailsPage, homePage } from "../controllers/homeController.js"
 import { aboutPage } from "../controllers/aboutController.js";
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const initWebRoute = (app) => {
     router.get(`/`, homePage);
     router.get(`/about`, aboutPage);
     router.get('/details/user/:userId', detailsPage);
+    router.get('/edit/user/:userId', userEditPage);
 
     router.post('/create-user', createUser);
     router.post('/edit-user', editUser);
